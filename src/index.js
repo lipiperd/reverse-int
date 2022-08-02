59c10fd046d576b2c4c26c7f8e2dbf2417b7ecd6
lipiperd <lipiperd@yandex.ru>
2022-08-03 02:24:32 +0400

    fix: fix bags

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 module.exports = function reverse(n) {
     let result = 0;
     while (n) {
-        result = result * 10 + n % 10;
-        n = Math.floor(n / 10);
+        result = result * 10 + Math.abs(n) % 10;
+        n = Math.floor(Math.abs(n) / 10);
     }
-
     return result;
 }
